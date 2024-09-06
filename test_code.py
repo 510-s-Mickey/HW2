51bb2abf7843d926ff4c32cfddd10446e997f245
Chris <100252884+chris-hzc@users.noreply.github.com>
2024-09-06 21:10:52 +0000

    test v2

--- test_code.py
+++ test_code.py
@@ -1,9 +1,11 @@
 from hw2_debugging import mergeSort
-def test_sorted_array():
-    assert mergeSort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
 
-def test_unsorted_array():
+def test_sorted1():
     assert mergeSort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
 
-# def test_empty_array():
-#     assert mergeSort([]) == []
+def test_sorted2():
+    assert mergeSort([38, 27, 43, 3, 9, 82, 10]) == [3, 9, 10, 27, 38, 43, 82]
+
+def test_sorted3():
+    assert mergeSort([5, -3, 7, -3, 2, -1, 5]) == [-3, -3, -1, 2, 5, 5, 7]
+
