4a121b2b0ad60c25510c6a6bd022fac3567d3c63
Hui Sun <123849556+HuiSunNCSU@users.noreply.github.com>
2024-09-10 12:41:10 -0400

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,23 +1,23 @@
 """
 This is a script test mergesort
 """
-from hw2_debugging import mergeSort
+from hw2_debugging import merge_sort
 
 def test_sorted1():
     """
     assert 
     """
-    assert mergeSort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
+    assert merge_Sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
 
 def test_sorted2():
     """
     assrt
     """
-    assert mergeSort([38, 27, 43, 3, 9, 82, 10]) == [3, 9, 10, 27, 38, 43, 82]
+    assert merge_sort([38, 27, 43, 3, 9, 82, 10]) == [3, 9, 10, 27, 38, 43, 82]
 
 def test_sorted3():
     """
     assert
     """
-    assert mergeSort([5, -3, 7, -3, 2, -1, 5]) == [-3, -3, -1, 2, 5, 5, 7]
+    assert merge_sort([5, -3, 7, -3, 2, -1, 5]) == [-3, -3, -1, 2, 5, 5, 7]
 
