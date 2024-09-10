f069df1d2da223da5063d6ede6e657b0ee63b28d
Hui Sun <123849556+HuiSunNCSU@users.noreply.github.com>
2024-09-10 12:32:01 -0400

    Update rand.py
    to pass pylint

--- rand.py
+++ rand.py
@@ -1,6 +1,12 @@
+"""
+This is a script to random array
+"""
 import subprocess
 
 def random_array(arr):
+    """
+    Function to generate random array
+    """
     shuffled_num = None
     for i in range(len(arr)):
         shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
