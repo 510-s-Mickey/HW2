b878c0fe87a914731fbc786fd39aa28f55afedfe
Hui Sun <123849556+HuiSunNCSU@users.noreply.github.com>
2024-09-10 12:35:26 -0400

    Update rand.py
    update rand as required by pylint

--- rand.py
+++ rand.py
@@ -7,8 +7,8 @@ def random_array(arr):
     """
     Function to generate random array
     """
-    shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    for index, _ in enumerate(arr):
+        # Adding check=True to ensure errors are caught
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
+        arr[index] = int(shuffled_num.stdout)
     return arr
